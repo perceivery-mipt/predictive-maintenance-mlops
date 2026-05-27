8dfa0dd9c096985479d6ea683e06f89f4881ce45
af_perceivery <af@perceivery.ru>
2026-05-27 14:33:46 +0300

    Configure API model loading from MLflow tracking URI

--- app/model_loader.py
+++ app/model_loader.py
@@ -1,10 +1,17 @@
 import os
 
+import mlflow
 import mlflow.pyfunc
 
 
 MODEL_NAME = os.getenv("MODEL_NAME", "predictive-maintenance-model")
 MODEL_ALIAS = os.getenv("MODEL_ALIAS", "champion")
+MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
+
+
+def configure_mlflow() -> None:
+    if MLFLOW_TRACKING_URI:
+        mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
 
 
 def get_model_uri() -> str:
@@ -12,5 +19,6 @@ def get_model_uri() -> str:
 
 
 def load_model():
+    configure_mlflow()
     model_uri = get_model_uri()
     return mlflow.pyfunc.load_model(model_uri)
